dbc4d235076d91d7f4c660f156331381fb6c1f4b
INYONG KIM <ohnyong87@gmail.com>
2024-01-12 14:54:05 +0900

    feat : 기본적인 include로 레이아웃 참조시키기 방식

--- app.js
+++ app.js
@@ -6,31 +6,31 @@ const port = 3000
 app.set('view engine', 'ejs');
 app.set('views', './views')
 
-//Root URL('/') 경로에 대한 Get 요청
 app.get('/', function (req, res) {
   res.render('index');
 })
 
-//특정 URL 경로에 대한 Get 요청 - 1
+app.get('/profile', function (req, res) {
+  res.render('profile');
+})
+
 app.get('/boards', function (req, res) {
   res.render('boards');
 })
 
-//특정 URL 경로에 대한 Get 요청 - 2
 app.get('/users', function (req, res) {
   res.render('users');
 })
 
-//특정 URL 경로에 대한 Get 요청의 JSON 형식 응답 - 1
 app.get('/api/boards/1', function (req, res) {
   res.send('{"id": 1, "title": "Title of the novel", "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum ha.."}');
 })
 
-//특정 URL 경로에 대한 Get 요청의 JSON 형식 응답 - 2
 app.get('/api/users/1', function (req, res) {
   res.send('{"id": 1, "username": "Fred", "role": "USER"}');
 })
 
+
 app.listen (port, () =>
-  console. log(`Example app listening on port ${port},`)
+  console. log(`서버 실행. 접속주소 : http://localhost:${port}`)
 )
