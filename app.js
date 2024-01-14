9154cc663462c3b8f850db7e489aee6328a65ce0
INYONG KIM <ohnyong87@gmail.com>
2024-01-14 18:58:44 +0900

    feat : MySQL 데이터베이스 커넥션 생성

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const app = express()
 const port = 3000
@@ -11,6 +13,26 @@ app.set('views', './views')
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
+// MySQL connection
+const connectionPool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  insecureAuth: true,
+});
+
+// MySQL connection check
+connectionPool.getConnection((err, connection) => {
+  if (err) {
+    console.error('MySQL에 연결 중 에러 발생:', err);
+  } else {
+    console.log('MySQL에 연결되었습니다.');
+    // 사용이 끝난 경우 연결을 풀에 반환합니다.
+    connection.release();
+  }
+});
 
 app.get('/', function (req, res) {
   res.render('index');
